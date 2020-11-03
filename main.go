4f672b201431d2992e712018f68001c56079fe1a
sww <13979062948@163.com>
2020-11-03 18:56:20 +0800

    删除注释

--- main.go
+++ main.go
@@ -7,9 +7,6 @@ import (
 
 func main() {
 	iptvConfig := lib.LoadCfg(util.DefaultCfgPath)
-	//ncuUser := iptvConfig.NcuUser
-	//appConfig := iptvConfig.AppConfig
 	factory := lib.IptvFactory{iptvConfig}
 	factory.CreateTasks()
-	//lib.Run(&ncuUser, &appConfig)
 }
